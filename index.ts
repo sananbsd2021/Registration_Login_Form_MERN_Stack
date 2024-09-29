895f045f9e2fd068936c0712da0495eb13048ace
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-09-29 21:26:32 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,12 +3,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const FormDataModel = require ('./models/FormData');
 
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/practice_mern');
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 app.post('/register', (req, res)=>{
     // To post / insert data into database
